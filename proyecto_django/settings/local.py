27f32babf0f00ca310e61acd4cae728f682e2974
DanielD4vil4x <danieldavila0405@gmail.com>
2025-06-05 16:13:54 -0500

    Update local.py

--- proyecto_django/settings/local.py
+++ proyecto_django/settings/local.py
@@ -19,8 +19,8 @@ DATABASES = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS=[BASE_DIR /'static']
+STATIC_URL = '/static/'
+STATICFILES_DIRS = BASE_DIR / 'static'
 STATIC_ROOT = BASE_DIR / 'staticfiles'  # Carpeta donde se copiarán
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
